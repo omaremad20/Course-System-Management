1dd56576e9910cf7ff4aecaec2b7a66f4364f80f
omaremad20 <oe747745@gmail.com>
2026-06-24 19:46:03 +0300

    Initial Commit

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('Course-System-Management');
+}

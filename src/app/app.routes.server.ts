accd43499a4f64cbd6bd7a5723f6d1de54aa9510
omaremad20 <oe747745@gmail.com>
2026-06-26 01:38:26 +0300

    fix: resolve SSR prerender error for dynamic routes with param

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: 'course-details/:courseId', renderMode: RenderMode.Server },
+  { path: 'edit-course/:courseId', renderMode: RenderMode.Server },
+
+  { path: '**', renderMode: RenderMode.Prerender },
 ];

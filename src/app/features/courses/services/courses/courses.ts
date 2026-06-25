d45742b231fef39862753b8d1287bf24a4531c32
omaremad20 <oe747745@gmail.com>
2026-06-26 01:28:03 +0300

    update : courses services to make filters & sorting & pagination

--- src/app/features/courses/services/courses/courses.ts
+++ src/app/features/courses/services/courses/courses.ts
@@ -1,36 +1,54 @@
-  import { HttpClient } from '@angular/common/http';
-  import { inject, Service } from '@angular/core';
-  import { Observable } from 'rxjs';
-  import { environment } from '../../../../../enviroments/enviroment';
-  import { ICourse } from '../../models/ICourse';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../../../../enviroments/enviroment';
+import { ICourse } from '../../models/ICourse';
 
-  @Service()
-  export class CoursesService {
-    private http = inject(HttpClient);
-    private readonly URL = environment.API_URL;
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  private http = inject(HttpClient);
+  private readonly URL = environment.API_URL;
 
-    // GET ALL COURSES
-    fetchCourses(): Observable<ICourse[]> {
-      return this.http.get<ICourse[]>(`${this.URL}/courses`);
-    }
+  fetchCourses(paramsObj: {
+    page: number;
+    limit: number;
+    search?: string;
+    status?: string;
+    sort?: string;
+    order?: 'asc' | 'desc';
+  }): Observable<{ data: ICourse[]; totalCount: number }> {
+    let params = new HttpParams()
+      .set('_page', paramsObj.page.toString())
+      .set('_limit', paramsObj.limit.toString());
 
-    // GET SPECIFIC COURSE DETAILS
-    fetchSpecificCourseDetails(courseId: string): Observable<ICourse> {
-      return this.http.get<ICourse>(`${this.URL}/courses/${courseId}`);
-    }
+    if (paramsObj.search) params = params.set('q', paramsObj.search);
+    if (paramsObj.status) params = params.set('status', paramsObj.status);
+    if (paramsObj.sort) params = params.set('_sort', paramsObj.sort);
+    if (paramsObj.order) params = params.set('_order', paramsObj.order);
 
-    // CREATE NEW COURSE
-    createNewCourse(course: ICourse): Observable<ICourse> {
-      return this.http.post<ICourse>(`${this.URL}/courses`, course);
-    }
-
-    // UPDATE COURSE (the sent course object will replaced with old course object)
-    updateCourse(course: ICourse, courseId: string): Observable<ICourse> {
-      return this.http.put<ICourse>(`${this.URL}/courses/${courseId}`, course);
-    }
-
-    // DELETE COURSE
-    deleteCourse(courseId: string) {
-      return this.http.delete<ICourse>(`${this.URL}/courses/${courseId}`);
-    }
+    return this.http.get<ICourse[]>(`${this.URL}/courses`, { params, observe: 'response' }).pipe(
+      map((response) => ({
+        data: response.body || [],
+        totalCount: Number(response.headers.get('X-Total-Count')) || 0,
+      })),
+    );
   }
+
+  fetchSpecificCourseDetails(courseId: string): Observable<ICourse> {
+    return this.http.get<ICourse>(`${this.URL}/courses/${courseId}`);
+  }
+
+  createNewCourse(course: ICourse): Observable<ICourse> {
+    return this.http.post<ICourse>(`${this.URL}/courses`, course);
+  }
+
+  updateCourse(course: ICourse, courseId: string): Observable<ICourse> {
+    return this.http.put<ICourse>(`${this.URL}/courses/${courseId}`, course);
+  }
+
+  deleteCourse(courseId: string): Observable<any> {
+    return this.http.delete<any>(`${this.URL}/courses/${courseId}`);
+  }
+}

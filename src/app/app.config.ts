04bd1afa290d9520a99cb751fb4fd4feccc93677
omaremad20 <oe747745@gmail.com>
2026-06-25 23:39:29 +0300

    setup : routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes, withViewTransitions()), provideClientHydration(),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { CourseList } from './features/courses/pages/course-list/course-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list',
+    title: 'CMS List',
+  },
+  {
+    title: 'CMS List',
+    path: 'list',
+    component: CourseList,
+  },
+  {
+    title: 'Course Details - CMS',
+    path: 'course-details/:courseId',
+    loadComponent: () => import('./features/courses/pages/course-details/course-details').then((c) => c.Coursedetails),
+  },
+  {
+    title: 'Edit Course - CMS',
+    path: 'edit-course/:courseId',
+    loadComponent: () => import('./features/courses/pages/course-form/course-form').then((c) => c.CourseForm),
+  },
+  {
+    title: 'Add Course - CMS',
+    path: 'add-course',
+    loadComponent: () => import('./features/courses/pages/course-form/course-form').then((c) => c.CourseForm),
+  },
+  {
+    title: '404 Page Not Found - CMS',
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found').then((c) => c.NotFound),
+  },
+];

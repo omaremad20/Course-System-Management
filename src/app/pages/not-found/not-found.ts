32c5ef88e98c8f01cfbc4434094dfc4c04d4e185
omaremad20 <oe747745@gmail.com>
2026-06-26 01:43:24 +0300

    add not found page to handle invalid urls

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MatIconModule, RouterLink, MatButtonModule],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
-export class NotFound {}
+export class NotFound {
+  private readonly _Location = inject(Location);
+
+  goBack() {
+    this._Location.back();
+  }
+}

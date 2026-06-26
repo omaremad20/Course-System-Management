1da873607c0d98dbff0a978f9b6529cdea230a93
omaremad20 <oe747745@gmail.com>
2026-06-27 01:30:18 +0300

    update : domain for api url

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  PROD_API_URL_BACKEND: 'https://course-system-management-backend-production.up.railway.app',
+  PROD_API_URL_BACKEND: 'course-management-dashboard-backend-production.up.railway.app',
   DEV_API_URL_BACKEND: 'http://localhost:3000',
 };

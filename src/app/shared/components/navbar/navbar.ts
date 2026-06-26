1531cd8dacf3f57b088c790e35afedf3b7a288a3
omaremad20 <oe747745@gmail.com>
2026-06-26 23:30:58 +0300

    remove unused imports

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIcon } from "@angular/material/icon";
-import { MatAnchor } from "@angular/material/button";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, MatIcon, MatAnchor],
+  imports: [RouterLink, RouterLinkActive, MatIcon, ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })

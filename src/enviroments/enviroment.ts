416b583d998c03ceb232739b10502162d48e718f
omaremad20 <oe747745@gmail.com>
2026-06-27 01:34:33 +0300

    fix : invalid API Url

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  PROD_API_URL_BACKEND: 'course-management-dashboard-backend-production.up.railway.app',
+  PROD_API_URL_BACKEND: 'https://course-management-dashboard-backend-production.up.railway.app/',
   DEV_API_URL_BACKEND: 'http://localhost:3000',
 };

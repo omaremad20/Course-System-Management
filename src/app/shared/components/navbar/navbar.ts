d8cf665f9c592bf875012be677989bd12bfd2dfd
omaremad20 <oe747745@gmail.com>
2026-06-26 20:22:37 +0300

    redesign navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIcon, MatAnchor],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
